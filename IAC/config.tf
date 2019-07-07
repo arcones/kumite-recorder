f6ea0f575bc4bdb47d2234328e1c523008473eaf
Marta Arcones <arcones@live.com>
2019-07-07 10:36:21 +0200

    Created terraform state bucket

---
+++ IAC/config.tf
@@ -0,0 +1,23 @@
+terraform {
+  backend s3 {
+    bucket  = "kumite-state"
+    region  = "eu-central-1"
+    key     = "kumite-recorder/global/s3/terraform.tfstate"
+    encrypt = true
+  }
+}
+
+provider aws {
+  region  = "eu-central-1"
+  //TODO pending to use specific profile
+}
+
+resource aws_s3_bucket kumite_state {
+  bucket = "kumite-state"
+  versioning {
+    enabled = true
+  }
+  lifecycle {
+    prevent_destroy = true
+  }
+}
