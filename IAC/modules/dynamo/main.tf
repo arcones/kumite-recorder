9cc47e5af38289eebba1075802866b1cbd447aad
Marta Arcones <marta.arconesrodriguez@zooplus.com>
2019-07-15 18:33:25 +0200

    ApiGateway implementation

--- IAC/modules/dynamo/main.tf
+++ IAC/modules/dynamo/main.tf
@@ -2,23 +2,11 @@ resource aws_dynamodb_table kumite_records {
   name         = "Exchange"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "Id"
-  range_key    = "Timestamp"
 
   attribute {
     name = "Id"
     type = "N"
   }
-  //
-  //  attribute {
-  //    name = "RequestOrResponse"
-  //    type = "B"
-  //  }
-
-  attribute {
-    name = "Timestamp"
-    type = "N"
-  }
-
   tags = {
     Application = "kumite-recorder"
   }
--- IAC/modules/lambda/payload.py
+++ IAC/modules/lambda/payload.py
@@ -1,6 +1,17 @@
 import boto3
+import time
+
 
 def handler(event, context):
     dynamodb = boto3.client('dynamodb')
-    dynamodb.put_item(TableName='Exchange', Item={'Id':{'N':'12345'},'Request':{'Path':{'S': 'cats'}, 'Method':{'S': 'GET'}, 'Response':{'Status':{'N':'200'}}}})
-
+    dynamodb.put_item(TableName='Exchange',
+                      Item={
+                          'Id': time.time(),
+                          'Request': {
+                              'Path': 'cats',
+                              'Method': 'GET'
+                          },
+                          'Response': {
+                              'Status': 200
+                          }
+                      })
