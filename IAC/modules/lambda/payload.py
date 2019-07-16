ef162d22f196e627efe6a437e45d3d34f340070b
Marta Arcones <marta.arconesrodriguez@zooplus.com>
2019-07-16 10:15:17 +0200

    Fix syntax of dynamo object

--- IAC/modules/lambda/payload.py
+++ IAC/modules/lambda/payload.py
@@ -6,7 +6,7 @@ def handler(event, context):
     dynamodb = boto3.client('dynamodb')
     dynamodb.put_item(TableName='Exchange',
                       Item={
-                          "Id": {"N": "int(time.time())"},
+                          "Id": {"N": str(int(time.time()))},
                           "Request": {
                               "Path": {"S": "cats"},
                               "Method": {"S": "GET"}
