de43176a452adbbd0371ea1ab9504942ce27bf9c
Marta Arcones <marta.arconesrodriguez@zooplus.com>
2019-07-16 13:40:43 +0200

    Coding lambda payload

--- IAC/modules/lambda/payload.py
+++ IAC/modules/lambda/payload.py
@@ -1,14 +1,26 @@
 import boto3
 import time
 import logging
+import json
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-def handler(event, context):
-    path = event["path"]
-    method = event["method"]
-    status = event["status"]
+def handler(event, context):##Si no estoy usando el contexto, igual se puede borrar
+    logger.info("The event is {}".format(event))
+    logger.info("The event's body is {}".format(event["body"]))
+
+
+    jsonBody = json.loads(event["body"])
+
+    logger.info("The event's body's path is {}".format(jsonBody["path"]))
+    logger.info("The event's body's method is {}".format(jsonBody["method"]))
+    logger.info("The event's body's status is {}".format(jsonBody["status"]))
+
+
+    path = jsonBody["path"]
+    method = jsonBody["method"]
+    status = jsonBody["status"]
 
     dynamodb = boto3.client('dynamodb')
     dynamodb.put_item(TableName='Exchange',
