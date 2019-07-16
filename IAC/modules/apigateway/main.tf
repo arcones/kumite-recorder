7d0371c14e2a48c4cf2313fb106087dc8a480ccd
Marta Arcones <marta.arconesrodriguez@zooplus.com>
2019-07-16 10:43:21 +0200

    ApiGateway implementation

--- IAC/modules/apigateway/main.tf
+++ IAC/modules/apigateway/main.tf
@@ -11,7 +11,7 @@ resource aws_api_gateway_resource proxy {
 resource aws_api_gateway_method proxy {
   rest_api_id = aws_api_gateway_rest_api.kumite_writer_api.id
   resource_id = aws_api_gateway_resource.proxy.id
-  http_method = "ANY"
+  http_method = "POST"
   authorization = "NONE"
 }
 
@@ -28,7 +28,7 @@ resource aws_api_gateway_integration lambda {
 resource aws_api_gateway_method proxy_root {
   rest_api_id = aws_api_gateway_rest_api.kumite_writer_api.id
   resource_id = aws_api_gateway_rest_api.kumite_writer_api.root_resource_id
-  http_method = "ANY"
+  http_method = "POST"
   authorization = "NONE"
 }
 
